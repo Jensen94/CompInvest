23c6ca3ffba06f97d7bd31015990c29705fdd6c0
kchowan <kchowaniec33@hotmail.com>
2014-12-29 09:48:32 -0600

    Added New feature
    3 variables

--- Learning Python/testing_code.py
+++ Learning Python/testing_code.py
@@ -2,8 +2,8 @@ title = "The First Test"
 directions = "sit back and relax a while"
 
 """attempting to run simple functions in command prompt"""
-def multiplier(x,y):
-	answer = int(x)*int(y)
+def multiplier(x,y,z):
+	answer = int(x)*int(y)*int(z)
 	return answer
 	
 def prompt_printer():
@@ -11,7 +11,8 @@ def prompt_printer():
 	print (directions)
 	x = input('Please input one number to be muliplited: ')
 	y = input('Please eneter a second number: ')
-	answer = multiplier(x,y)
-	print ("Your input " + str(x) + " and " + str(y) + " is equal to " + str(answer))
+	z = input('Please enter a third number: ')
+	answer = multiplier(x,y,z)
+	print ("Your input " + str(x) + " and " + str(y) + " and " + str(z) + " is equal to " + str(answer))
 	
 prompt_printer()	
